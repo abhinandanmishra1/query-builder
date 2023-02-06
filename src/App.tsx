39de8badcc90c96b4296827a1091afbb8891d49b
abhinandanmishra1 <abhinandanmishra360@gmail.com>
2023-02-06 15:51:18 +0530

    feat: home-page-basic ui

--- src/App.tsx
+++ src/App.tsx
@@ -24,16 +24,20 @@ const RuleGroups: React.FC = () => {
 				pauseOnHover
 				theme="dark"
 			/>
-			<div className={`h-screen bg-black `}>
-				<button
-					onClick={() => setToggle(true)}
-					disabled={toggle}
-					className={`bg-[#5C61F0] hover:bg-[#645CBB] text-white font-bold py-2 px-4 rounded ${
-						toggle && "opacity-70"
-					}`}
-				>
-					Build Query
-				</button>
+			<div className={`h-screen bg-black p-4 ${toggle && 'opacity-90'}`}>
+				<div className={`text-white border border-[#404348] w-11/12 m-auto p-4 rounded shadow shadow-lg ${toggle && 'opacity-90'}`}>
+					<h3 className="text-lg m-1">Build your query</h3>
+					<p className="text-[#404348] text-sm m-1">It is a platform to provide easiness for building queries.</p>
+					<button
+						onClick={() => setToggle(true)}
+						disabled={toggle}
+						className={`bg-[#5C61F0] hover:bg-[#5C61F099] text-white font-bold py-2 px-4 rounded ${
+							toggle && "opacity-70"
+						}`}
+					>
+						Build Query
+					</button>
+				</div>
 			</div>
 		</>
 	);
